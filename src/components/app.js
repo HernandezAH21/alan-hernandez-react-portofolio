d27bb0a556c1fc72541ce39a81b4bf431c691ef2
HernandezAH21 <serrato.alan21@gmail.com>
2020-04-06 16:08:17 -0500

    added styles to portfolio app

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
-import moment from "moment";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-export default class App extends Component {
+import NavigationContainer from './navigation/navigation-container';
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Blog from "./pages/blog";
+import PortfolioDetail from "./portfolio/portfolio-details";
+import NoMatch from "./pages/no-match";
+
+export default class App extends Component {  
   render() {
     return (
-      <div className='app'>
-        <h1>React Portofolio</h1>
-        <h2>By: Alan Hernandez Programming Student</h2>
-      <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
+      <div className='container'>        
+        <Router>
+          <div>
+            <h2> Alan Hernandez Programming Student</h2>
+            <NavigationContainer />
+
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about-me" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/blog" component={Blog} />
+              <Route path="/portfolio/:slug" component={PortfolioDetail} />
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+        </Router>
       </div>
     );
   }
