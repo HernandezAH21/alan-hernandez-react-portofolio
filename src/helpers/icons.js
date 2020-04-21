431550047b42cb95c51cbf7490b28192362b4fe1
Alan Hernandez <serrato.alan21@gmail.com>
2020-04-21 16:44:45 -0500

    Update 21 april 2020

---
+++ src/helpers/icons.js
@@ -0,0 +1,14 @@
+import {
+    faTrash,
+    faSignOutAlt,
+    faEdit,
+    faSpinner,
+    faDragon
+  } from "@fortawesome/free-solid-svg-icons";
+  import { library } from "@fortawesome/fontawesome-svg-core";
+  
+  const Icons = () => {
+    return library.add(faTrash, faSignOutAlt, faEdit, faSpinner, faDragon);
+  };
+  
+  export default Icons;
